9e412040644b34228eff8ba4e96cf31194f10804
ErAshishChawla <ashishchawladev@gmail.com>
2023-06-14 15:22:04 -0400

    creating version 1

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
 print("This is a new change on 10th June 2023")
 print("This is line 2")
 print("This is change from feature branch")
+
+print("This is version 1")
--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,5 @@
 This is a change from feature branch
 
 Another change 
+
+This is version 1
